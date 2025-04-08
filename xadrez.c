30db907edee24346255d017bb5aedd743e62eae4
Cristhyan Capetini <capetini.cris@gmail.com>
2025-04-08 00:51:40 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,64 @@
 #include <stdio.h>
 
-int main() {
-    // Movimento da Torre (usando for)
-    printf("Movimento da Torre:\n");
-    int casas_torre = 5;
-    for (int i = 0; i < casas_torre; i++) {
+// Função recursiva para o movimento da Torre
+void moverTorreRecursivo(int casas) {
+    if (casas > 0) {
         printf("Direita\n");
+        moverTorreRecursivo(casas - 1);
     }
-    printf("\n");
+}
 
-    // Movimento do Bispo (usando while)
-    printf("Movimento do Bispo:\n");
-    int casas_bispo = 5;
-    int j = 0;
-    while (j < casas_bispo) {
+// Função recursiva para o movimento do Bispo
+void moverBispoRecursivo(int casas) {
+    if (casas > 0) {
         printf("Cima, Direita\n");
-        j++;
+        moverBispoRecursivo(casas - 1);
+    }
+}
+
+// Função recursiva para o movimento da Rainha
+void moverRainhaRecursivo(int casas) {
+    if (casas > 0) {
+        printf("Esquerda\n");
+        moverRainhaRecursivo(casas - 1);
+    }
+}
+
+int main() {
+    // Movimento da Torre (usando recursão)
+    printf("Movimento da Torre (Recursivo):\n");
+    int casas_torre = 5;
+    moverTorreRecursivo(casas_torre);
+    printf("\n");
+
+    // Movimento do Bispo (usando recursão e loops aninhados)
+    printf("Movimento do Bispo (Recursivo e Loops Aninhados):\n");
+    int casas_bispo_vertical = 5;
+    for (int i = 0; i < casas_bispo_vertical; i++) {
+        for (int j = 0; j < 1; j++) { // Loop interno para movimento horizontal (diagonal direita)
+            printf("Cima, Direita\n");
+        }
     }
     printf("\n");
 
-    // Movimento da Rainha (usando do-while)
-    printf("Movimento da Rainha:\n");
+    // Movimento da Rainha (usando recursão)
+    printf("Movimento da Rainha (Recursivo):\n");
     int casas_rainha = 8;
-    int k = 0;
-    do {
-        printf("Esquerda\n");
-        k++;
-    } while (k < casas_rainha);
+    moverRainhaRecursivo(casas_rainha);
+    printf("\n");
+
+    // Movimento do Cavalo (usando loops aninhados complexos)
+    printf("Movimento do Cavalo (Loops Aninhados Complexos - Cima e Direita em L):\n");
+    int movimentos_cavalo = 1; // Controla quantas vezes o movimento em L será realizado
+
+    for (int m = 0; m < movimentos_cavalo; m++) {
+        for (int cima = 0; cima < 2; cima++) {
+            printf("Cima\n");
+        }
+        for (int direita = 0; direita < 1; direita++) {
+            printf("Direita\n");
+        }
+    }
     printf("\n");
 
     return 0;
